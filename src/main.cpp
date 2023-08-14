ab0f0ac174417b136136052a6255b02694aebc3b
David Ryan <davidryan0119@gmail.com>
2023-08-14 15:53:56 -0400

    Bug Fix - Fixed SEGFAULT in Wine with Read Stream Redirection

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <windows.h>
-#include <tlhelp32.h>
 
 #include "main.hpp"
 
@@ -16,7 +15,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  dwReason, LPVOID lpReserved)
         case DLL_THREAD_DETACH:
             break;
         case DLL_PROCESS_DETACH:
-            FreeConsole();
+//            FreeConsole();
             break;
         default: 
             std::cout << "An Unexpected Error has Occurred." << std::endl;
@@ -27,11 +26,23 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  dwReason, LPVOID lpReserved)
 
 int OnAttach() 
 {
-    AllocConsole();
+    if(AllocConsole())
+    {
+        SetConsoleOutputCP(65001); // UTF-8
 
-    freopen_s((FILE**)stdin, "CONIN$", "r", stdin);
-    freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
-    freopen_s((FILE**)stderr, "CONOUT$", "w", stderr);
+        freopen_s(reinterpret_cast<FILE**>(&std::cin), "CONIN$", "r", stdin);
+        freopen_s(reinterpret_cast<FILE**>(&std::cout), "CONOUT$", "w", stdout);
+
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_CURSOR_INFO CursoInfo;
+        CursoInfo.dwSize = 1;
+        CursoInfo.bVisible = false;
+        SetConsoleCursorInfo(hConsole, &CursoInfo);
+
+        std::cout << "Hello, world!" << std::endl;
+    }
+    else return -1;
+
 
     return 0;
 }
