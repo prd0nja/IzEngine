a5b2f1c9642bc8ed6c9b2cfa48c5306547eabcf4
Dualiteee <lordalite@gmail.com>
2024-05-31 13:09:01 +0200

    Fixed multiple notification bug & small change (optional)

--- src/Engine/Renderer/Backends/ImGUI/Drawing/Notification.cpp
+++ src/Engine/Renderer/Backends/ImGUI/Drawing/Notification.cpp
@@ -16,10 +16,11 @@ namespace IzEngine
 
 		ImDrawList* draw = ImGui::GetBackgroundDrawList();
 		int position = 50;
+		int count = 0;
 
 		for (const auto& notification : List)
 		{
-			Window window("##Notfication");
+			Window window(std::format("##Notification {}", count));
 			window.SetRect(0, position, 140, 20);
 			window.Begin(ImGuiWindowFlags_Notification);
 
@@ -30,10 +31,11 @@ namespace IzEngine
 			draw->AddRectFilled({ pos.x + size.x, pos.y }, { pos.x + size.x + 5, pos.y + size.y },
 				IM_COL32(140, 20, 252, 255));
 
-			ImGui::TextWrapped(notification.message.c_str());
+			ImGui::TextWrapped("IzEngine: %s", notification.message.c_str());
 			window.End();
 
-			position += size.y + 10;
+			position += size.y;
+			count++;
 		}
 		std::erase_if(List,
 			[](const Notification& notification)
